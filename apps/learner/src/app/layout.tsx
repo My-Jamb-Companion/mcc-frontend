ccf0f5b825d8c661dcf0e33d1663088e5cb11fa7
Elvis <Elvisraphael00@gmail.com>
2026-04-09 17:36:41 +0100

    feat: refactor layout and styles for improved font handling and consistency

--- apps/learner/src/app/layout.tsx
+++ apps/learner/src/app/layout.tsx
@@ -1,7 +1,18 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Bagel_Fat_One, Geist, Geist_Mono, Inter} from "next/font/google";
 import "./globals.css";
-import { AppProviders } from "@/src/providers/AppProvider";
+import {AppProviders} from "@/src/providers/AppProvider";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const bagel = Bagel_Fat_One({
+  variable: "--font-bagel",
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +37,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.variable} ${bagel.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="h-full flex flex-col">
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
