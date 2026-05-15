20de1a7ee9fc4cceeec5da76041a739080df26a0
Elvis <Elvisraphael00@gmail.com>
2026-05-15 08:42:44 +0100

    feat: implement authentication and onboarding redirection for dashboard layout

--- apps/learner/src/app/(dashboard)/layout.tsx
+++ apps/learner/src/app/(dashboard)/layout.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useAuth } from "@mcc/features";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import {RoleLayout} from "@/src/components/RoleLayout";
+import {useAuth} from "@mcc/features";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { isAuthenticated, hydrated, user } = useAuth();
+  const {isAuthenticated, hydrated, user} = useAuth();
+
   const router = useRouter();
 
   useEffect(() => {
@@ -21,7 +23,21 @@ export default function DashboardLayout({
     }
   }, [hydrated, isAuthenticated, user, router]);
 
-  if (!hydrated || !isAuthenticated || (user && !user.is_onboarded)) return null;
+  if (!hydrated || !isAuthenticated || (user && !user.is_onboarded))
+    return null;
 
-  return <>{children}</>;
+  return (
+    <RoleLayout allowedRoles={["student"]}>
+      <div className="flex flex-col h-screen">
+        <div
+          className="grid max-sm:grid-cols-1 h-full relative pr-12 max-sm:px-4 overflow-hidden"
+          style={{gridTemplateColumns: "auto 1fr"}}
+        >
+          <div className="overflow-y-auto col-start-2 pl-30 max-sm:pl-0 scrollbar-hide">
+            {children}
+          </div>
+        </div>
+      </div>
+    </RoleLayout>
+  );
 }
