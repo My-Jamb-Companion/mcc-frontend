4c1a5218bd41e52fbcf3584a7b086e6a65bfd812
Elvis <Elvisraphael00@gmail.com>
2026-05-25 15:20:31 +0100

    del

--- apps/learner/src/features/components/loadingCircle.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingCircle({className}: {className?: string}) {
-  return (
-    <div
-      className={`border-6 border-t-transparent border-black dark:border-white dark:border-t-transparent p-5 rounded-full animate-spin w-fit ${className || ""}`}
-    />
-  );
-}
