d0480c483b4bda68213593dc903ac21d35342b48
Elvis <Elvisraphael00@gmail.com>
2026-08-03 16:36:10 +0100

    feat: added all courses page components and dummy data for courses and empty state test case

---
+++ apps/admin/src/app/(admin)/dashboard/courses/page.tsx
@@ -0,0 +1,5 @@
+import Courses from "@/src/features/courses/components/Courses";
+
+export default function coursesPage() {
+  return <Courses />;
+}
--- apps/admin/src/features/courses/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./components/CourseList";
-export * from "./components/CreateCourseButton";
