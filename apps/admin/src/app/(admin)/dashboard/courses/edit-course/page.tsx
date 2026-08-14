89ee2172283b77e00ae4083088a87eac2e91fa98
Elvis <Elvisraphael00@gmail.com>
2026-08-14 17:33:44 +0100

    feat: add dynamic export to EditCoursePage component

--- apps/admin/src/app/(admin)/dashboard/courses/edit-course/page.tsx
+++ apps/admin/src/app/(admin)/dashboard/courses/edit-course/page.tsx
@@ -1,5 +1,7 @@
 import EditCourse from "@/src/features/courses/components/EditCourse";
 
+export const dynamic = "force-dynamic";
+
 export default function EditCoursePage() {
   return <EditCourse />;
 }
